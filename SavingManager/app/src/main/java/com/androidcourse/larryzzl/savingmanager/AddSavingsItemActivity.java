0884dd60b237c06dabf3856997ebd7016f3700bf
Zunlin Zhang <larryzzl@gmail.com>
2017-03-30 15:10:40 +0800

    fill all ui element

--- SavingManager/app/src/main/java/com/androidcourse/larryzzl/savingmanager/AddSavingsItemActivity.java
+++ SavingManager/app/src/main/java/com/androidcourse/larryzzl/savingmanager/AddSavingsItemActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class AddSavingsItemActivity extends AppCompatActivity {
@@ -18,12 +17,46 @@ public class AddSavingsItemActivity extends AppCompatActivity {
     }
 
     private void setupUI() {
-        TextView txtBank = (TextView)findViewById(R.id.txtBank);
-        EditText editBank = (EditText)findViewById(R.id.editBank);
 
+        EditText editBank = (EditText)findViewById(R.id.editBank);
         editBank.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(AddSavingsItemActivity.this, "Android Simple Toast", Toast.LENGTH_LONG).show();
+                Toast.makeText(AddSavingsItemActivity.this, "Input Bank", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        EditText editStartDate = (EditText)findViewById(R.id.editStartDate);
+        editStartDate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(AddSavingsItemActivity.this, "Input StartDate", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        EditText editEndDate = (EditText)findViewById(R.id.editEndDate);
+        editEndDate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(AddSavingsItemActivity.this, "Input EndDate", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        EditText editAmount = (EditText)findViewById(R.id.editAmount);
+        editAmount.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(AddSavingsItemActivity.this, "Input Amount", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        EditText editAnnual = (EditText)findViewById(R.id.editAnnual);
+        editAnnual.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(AddSavingsItemActivity.this, "Input Annual", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        EditText editExpected = (EditText)findViewById(R.id.editExpected);
+        editExpected.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(AddSavingsItemActivity.this, "Input Expected", Toast.LENGTH_LONG).show();
             }
         });
     }
