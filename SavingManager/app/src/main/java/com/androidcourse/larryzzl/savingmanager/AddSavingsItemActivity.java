76f246b970cdfa930f595771dcb278e3978ccef1
Zunlin Zhang <larryzzl@gmail.com>
2017-03-30 17:25:30 +0800

    Add date picker

--- SavingManager/app/src/main/java/com/androidcourse/larryzzl/savingmanager/AddSavingsItemActivity.java
+++ SavingManager/app/src/main/java/com/androidcourse/larryzzl/savingmanager/AddSavingsItemActivity.java
@@ -1,20 +1,47 @@
 package com.androidcourse.larryzzl.savingmanager;
 
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnFocusChangeListener;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class AddSavingsItemActivity extends AppCompatActivity {
+
+    int year;
+    int month;
+    int day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_savings_item);
 
+        Calendar calendar = Calendar.getInstance();
+        AddSavingsItemActivity.this.year = calendar.get(Calendar.YEAR);
+        AddSavingsItemActivity.this.month = calendar.get(Calendar.MONTH);
+        AddSavingsItemActivity.this.day = calendar.get(Calendar.DAY_OF_MONTH);
+
         setupUI();
     }
+
+    private void updateStartDate(int year, int monthOfYear, int dayOfMonth) {
+        EditText editStartDate = (EditText)findViewById(R.id.editStartDate);
+        editStartDate.setText(year + "-" + monthOfYear + "-" + dayOfMonth);
+    }
+
+    private void updateEndDate(int year, int monthOfYear, int dayOfMonth) {
+        EditText editEndDate = (EditText)findViewById(R.id.editEndDate);
+        editEndDate.setText(year + "-" + monthOfYear + "-" + dayOfMonth);
+    }
+
 
     private void setupUI() {
 
@@ -26,13 +53,46 @@ public class AddSavingsItemActivity extends AppCompatActivity {
         });
 
         EditText editStartDate = (EditText)findViewById(R.id.editStartDate);
-        editStartDate.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Toast.makeText(AddSavingsItemActivity.this, "Input StartDate", Toast.LENGTH_LONG).show();
+        editStartDate.setInputType(InputType.TYPE_NULL);
+        editStartDate.setShowSoftInputOnFocus(false);
+        editStartDate.setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+
+                    // Show date picker
+                    DatePickerDialog datePicker = new DatePickerDialog(AddSavingsItemActivity.this, new OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                            AddSavingsItemActivity.this.updateStartDate(year, monthOfYear, dayOfMonth);
+                        }
+                    }, AddSavingsItemActivity.this.year, AddSavingsItemActivity.this.month, AddSavingsItemActivity.this.day);
+                    datePicker.show();
+                }
             }
         });
 
         EditText editEndDate = (EditText)findViewById(R.id.editEndDate);
+        editEndDate.setInputType(InputType.TYPE_NULL);
+        editEndDate.setShowSoftInputOnFocus(false);
+        editEndDate.setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+
+                    // Show date picker
+                    DatePickerDialog datePicker = new DatePickerDialog(AddSavingsItemActivity.this, new OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                            AddSavingsItemActivity.this.updateEndDate(year, monthOfYear, dayOfMonth);
+                        }
+                    }, AddSavingsItemActivity.this.year, AddSavingsItemActivity.this.month, AddSavingsItemActivity.this.day);
+                    datePicker.show();
+                }
+            }
+        });
+
+
         editEndDate.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Toast.makeText(AddSavingsItemActivity.this, "Input EndDate", Toast.LENGTH_LONG).show();
